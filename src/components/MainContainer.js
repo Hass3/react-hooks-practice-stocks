7262b754e9d6f0723f275f510bba6153fa8284a2
Hass <hassmroueh@gmail.com>
2024-10-24 22:30:03 -0400

    practice stocks lab complete

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,18 +1,67 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import StockContainer from "./StockContainer";
 import PortfolioContainer from "./PortfolioContainer";
 import SearchBar from "./SearchBar";
 
+
+
 function MainContainer() {
+  const[stocks, setStocks]= useState([]);
+  const [portfolio, setPortfoilo] = useState([])
+  const [filteredStocks, setFilteredStocks] = useState([])
+  useEffect(()=>{
+    fetch("http://localhost:3001/stocks")
+    .then(r=>r.json())
+    .then(data=> {
+      setStocks(data);
+      setFilteredStocks(data);
+    })
+  }, [])
+
+  function handleBuyStock(stock){
+   const clickedStock = [...portfolio, stock]
+    setPortfoilo(clickedStock)
+  }
+
+  function handleSellStock(stok){
+    const newStocks = portfolio.filter(stock => stock !== stok)
+    setPortfoilo(newStocks)
+  }
+
+  function alphabetically(){
+    const alphabetically = [...stocks].sort((a, b) => a.ticker.localeCompare(b.ticker))
+    setStocks(alphabetically)
+  }
+
+  function price(){
+    const price = [...stocks].sort((a,b)=> a.price - b.price)
+    setStocks(price) 
+  }
+  const tech = ()=> {
+    const tech = filteredStocks.filter(stock => stock.type === "Tech");
+    setStocks(tech)
+  }
+
+ const sportsWear=()=>{
+  const sportsWear = filteredStocks.filter(stock => stock.type === "Sportswear");
+  setStocks(sportsWear)
+ }
+
+ const finance=()=>{
+  const finance = filteredStocks.filter(stock => stock.type === "Finance");
+  setStocks(finance)
+ }
+
+
   return (
     <div>
-      <SearchBar />
+      <SearchBar onPrice={price} onAlpha={alphabetically}  onTech={tech}  onSportsWear = {sportsWear}  onFinance ={finance}/>
       <div className="row">
         <div className="col-8">
-          <StockContainer />
+          <StockContainer stocks={stocks} handleBuyStock={handleBuyStock}/>
         </div>
         <div className="col-4">
-          <PortfolioContainer />
+          <PortfolioContainer stocks = {portfolio} handleSellStock={handleSellStock}/>
         </div>
       </div>
     </div>
@@ -20,3 +69,16 @@ function MainContainer() {
 }
 
 export default MainContainer;
+/**After the stocks are fetched, your job is to:
+
+Render all the stocks onto the page. The styling of how a Stock should look like is already in the Stock component.
+
+Allow a user to buy a stock by clicking on it and when it is bought, it should be added to MyPortfolio.
+
+Allow a user to sell a stock in their Portfolio by clicking on the stock and it should be removed from their Portfolio.
+
+Allow a user to sort the list of stocks alphabetically by the ticker name as well as by ascending price.
+
+Allow a user to filter stocks based on the type of the stock.
+
+Best of luck! */
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,6 +1,33 @@
-import React from "react";
+import React, {useState} from "react";
 
-function SearchBar() {
+function SearchBar({onPrice, onAlpha, onTech, onSportsWear, onFinance }) {
+  const [checked, setChecked] = useState('');
+  const [sort, setSort]=useState("");
+
+  function handleSortChange(e){
+    const selectedSort = e.target.value
+    setSort(selectedSort)
+    if (selectedSort === "Tech"){
+      onTech()
+     }
+     if (selectedSort=== "Sportswear"){
+      onSportsWear()
+     }
+     if (selectedSort === "Finance"){
+      onFinance()
+     }
+  }
+
+ function handleCheckChange(e){
+  setChecked(e.target.value)
+  if(e.target.value === "Alphabetically"){
+   onAlpha()
+  }
+  if (e.target.value === "Price"){
+    onPrice()
+  }
+ }
+
   return (
     <div>
       <strong>Sort by:</strong>
@@ -9,8 +36,8 @@ function SearchBar() {
           type="radio"
           value="Alphabetically"
           name="sort"
-          checked={null}
-          onChange={null}
+          checked={checked === "Alphabetically"}
+          onChange={handleCheckChange}
         />
         Alphabetically
       </label>
@@ -19,15 +46,15 @@ function SearchBar() {
           type="radio"
           value="Price"
           name="sort"
-          checked={null}
-          onChange={null}
+          checked={checked === "Price"}
+          onChange={handleCheckChange}
         />
         Price
       </label>
       <br />
       <label>
         <strong>Filter:</strong>
-        <select onChange={null}>
+        <select onChange={handleSortChange}>
           <option value="Tech">Tech</option>
           <option value="Sportswear">Sportswear</option>
           <option value="Finance">Finance</option>
